2db7d671cebf0a524bbb172e028763e0d872c953
Collin Kleest <43449691+collinkleest@users.noreply.github.com>
2019-08-12 15:50:13 -0400

    Add files via upload

--- alert.py
+++ alert.py
@@ -4,7 +4,7 @@ import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-
+#Reads and opens csv file with contacts and calls the sendEmail function
 def getPhones():
     with open('/home/pi/github_text/PowerOutageNotifier/phones.csv') as csvFile:
         csvReader = csv.reader(csvFile, delimiter=',')
@@ -14,7 +14,7 @@ def getPhones():
             name = row[2]
             sendEmail(phone, carrier, name)
 
-
+#Gets specific carrier input from csv file and outputs the sms gateway for that carrier
 def getCarrierAddr(carrier):
     carriers = {
         'verizon': 'vtext.com',
@@ -30,17 +30,21 @@ def getCarrierAddr(carrier):
     }
     return carriers.get(carrier.lower())
 
-
+#calls email  class with the inputs phone, carrier, and name
+#gets the 's' variable from the main function
 def sendEmail(phone, carrier, name):
     msg = Email(phone, carrier, name)
     s.send_message(msg.constructEmail())
 
+
 class Email:
+    # constucts email object
     def __init__(self, phone, carrier, name):
         self.phone = phone
         self.carrier = carrier
         self.name = name
-
+    
+    # builds the email
     def constructEmail(self):
         email = (str(self.phone) + '@') + (getCarrierAddr(self.carrier))
         finalmessage = (self.name+': \n'+message)
@@ -50,6 +54,7 @@ class Email:
         msg.attach(MIMEText(str(finalmessage), 'plain'))
         return msg
 
+#main function
 if __name__=="__main__":
     mailServer = "localhost"
     message = input()
